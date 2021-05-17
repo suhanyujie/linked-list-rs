14e6a63dfef4700eedc788ec126d7525212bd2f9
suhanyujie <suhanyujie@qq.com>
2021-05-17 12:30:57 +0800

    add iter and iter_mut for list

--- src/first.rs
+++ src/first.rs
@@ -1,12 +1,12 @@
 type Link<T> = Option<Box<Node<T>>>;
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Debug)]
 struct Node<T> {
     elem: T,
     next: Link<T>,
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Debug)]
 pub struct List<T> {
     head: Link<T>,
 }
@@ -56,6 +56,72 @@ impl<T> Drop for List<T> {
     }
 }
 
+// 给链表实现 Iterator trait。
+// 方便起见，给 List<T> 封装到元组结构体中
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+/// 实现 Iter
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_ref().map(|node| &**node),
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
+    }
+}
+
+/// 实现 IterMut
+pub struct IterMut<'a, T: 'a> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_mut().map(|node| &mut **node),
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -74,5 +140,28 @@ mod tests {
         list.push("Age: 22");
         assert_eq!(list.peek(), Some(&"Age: 22"));
         assert_eq!(list.peek_mut(), Some(&mut "Age: 22"));
+    }
+
+    #[test]
+    fn test_intoiterater() {
+        let mut list = List::new();
+        list.push(12);
+        list.push(28);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(28));
+        assert_eq!(iter.next(), Some(12));
+    }
+
+    #[test]
+    fn test_iter_mut() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+        let mut iter = list.iter_mut();
+        iter.for_each(|node| *node += 100);
+        assert_eq!(list.head.as_ref().unwrap().elem, 102);
     }
 }
